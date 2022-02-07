bdb6fce878b2fee68ed036d3de4f1f9648c932e8
Fenneig <ninetailedkot92@gmail.com>
2022-02-07 23:52:02 +0300

    HeroInput

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private CharacterController _controller;
+    [SerializeField] private float _speed;
+
+    private Vector3 _direction;
+
+    private void Awake()
+    {
+        _direction = new Vector3(0, 0, _speed);
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        _direction = new Vector3(direction.x, direction.y, _speed);
+    }
+
+    private void FixedUpdate()
+    {
+        _controller.Move(_direction * _speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField] private Hero _hero;
+
+    public void OnMovement(InputAction.CallbackContext context) => _hero.SetDirection(context.ReadValue<Vector2>());
+}
